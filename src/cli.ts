20f5143c63f0da67bc2514cb316b2816f7e0b2b0
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-10-04 00:10:19 -0400

    feat(cli): add flags to `dev` command

--- src/cli.ts
+++ src/cli.ts
@@ -2,13 +2,24 @@ import 'source-map-support/register'
 import { success } from 'misty'
 import color from 'kleur'
 import cac from 'cac'
+import * as vite from 'vite'
 
 const cli = cac('saus')
 
-cli.command('dev').action(async () => {
-  const { createServer } = require('./dev') as typeof import('./dev')
-  await createServer()
-})
+cli
+  .command('dev')
+  .option('--host [host]', `[string] specify hostname`)
+  .option('--port <port>', `[number] specify port`)
+  .option('--open [path]', `[boolean | string] open browser on startup`)
+  .option('--strictPort', `[boolean] exit if specified port is already in use`)
+  .option(
+    '--force',
+    `[boolean] force the optimizer to ignore the cache and re-bundle`
+  )
+  .action(async (options: vite.ServerOptions) => {
+    const { createServer } = require('./dev') as typeof import('./dev')
+    await createServer({ server: options })
+  })
 
 cli.command('build').action(async () => {
   const { build } = require('./build') as typeof import('./build')
