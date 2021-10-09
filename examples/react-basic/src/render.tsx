f9170319f5a5124ff5d02381322e415c5583fe41
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-10-08 21:01:36 -0400

    chore: update react-basic example

--- examples/react-basic/src/render.tsx
+++ examples/react-basic/src/render.tsx
@@ -5,20 +5,17 @@ import { App } from './App'
 render((module, { params }) => {
   const Page = module.default as React.ComponentType<any>
   return (
-    <html>
-      <head>
-        <title>Pokemon Wiki</title>
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <link
-          href="https://cdn.jsdelivr.net/npm/modern-normalize@1.1.0/modern-normalize.min.css"
-          rel="stylesheet"
-        />
-      </head>
-      <body>
-        <App>
-          <Page {...params} />
-        </App>
-      </body>
-    </html>
+    <App>
+      <Page {...params} />
+    </App>
   )
-})
+}).head(() => (
+  <head>
+    <title>Pokemon Wiki</title>
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/modern-normalize@1.1.0/modern-normalize.min.css"
+      rel="stylesheet"
+    />
+  </head>
+))
