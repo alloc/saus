c08ef5f42a7a2c7b5085a7318f9246357a3de0fa
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-10-14 13:45:47 -0400

    fix(cache): revive Buffer objects

--- src/build/cache.ts
+++ src/build/cache.ts
@@ -1,5 +1,6 @@
 import fs from 'fs'
 import path from 'path'
+import { isPlainObject } from 'is-plain-object'
 
 export const getCachePath = (root: string) =>
   path.join(root, 'node_modules/.saus')
@@ -15,7 +16,7 @@ export type Cache = {
 
 export const readCache = (cachePath: string) => {
   try {
-    return JSON.parse(fs.readFileSync(cachePath, 'utf8')) as Cache
+    return JSON.parse(fs.readFileSync(cachePath, 'utf8'), reviver) as Cache
   } catch {}
 }
 
@@ -23,4 +24,14 @@ export const writeCache = (cachePath: string, cache: Cache) => {
   // Ensure "transformIndexHtml" hooks are always called.
   cache.modules = cache.modules.filter(mod => !mod.id.endsWith('.html'))
   fs.writeFileSync(cachePath, JSON.stringify(cache))
+}
+
+function reviver(_key: string, value: any) {
+  if (
+    isPlainObject(value) &&
+    value.type === 'Buffer' &&
+    Array.isArray(value.data)
+  ) {
+    return Buffer.from(value.data)
+  }
 }
