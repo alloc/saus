882cdf4366eac1ee7bea03f68b880b70cd5cd226
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-10-29 16:21:55 -0400

    fix(dev): serve one page at a time
    …to avoid global state interference

--- src/plugins/serve.ts
+++ src/plugins/serve.ts
@@ -10,6 +10,7 @@ export function servePlugin(
   // any early page requests until it is.
   let init = defer<void>()
   let pageFactory: PageFactory
+  let requestQueue = Promise.resolve()
 
   return {
     name: 'saus:serve',
@@ -18,24 +19,26 @@ export function servePlugin(
       init.resolve()
     },
     configureServer: server => () =>
-      server.middlewares.use(async (req, res, next) => {
+      server.middlewares.use((req, res, next) => {
         const path = req.originalUrl!
-        await Promise.all([init, context.reloading])
-        await pageFactory.resolvePage(path, async (error, page) => {
-          if (page) {
-            const html = await server.transformIndexHtml(path, page.html)
-            res.setHeader('Content-Type', 'text/html; charset=utf-8')
-            res.setHeader('Content-Length', Buffer.byteLength(html))
-            res.writeHead(200)
-            res.write(html)
-            res.end()
-          } else if (error) {
-            onError(error)
-            res.writeHead(500)
-            res.end()
-          } else {
-            next()
-          }
+        requestQueue = requestQueue.then(async () => {
+          await Promise.all([init, context.reloading])
+          await pageFactory.resolvePage(path, async (error, page) => {
+            if (page) {
+              const html = await server.transformIndexHtml(path, page.html)
+              res.setHeader('Content-Type', 'text/html; charset=utf-8')
+              res.setHeader('Content-Length', Buffer.byteLength(html))
+              res.writeHead(200)
+              res.write(html)
+              res.end()
+            } else if (error) {
+              onError(error)
+              res.writeHead(500)
+              res.end()
+            } else {
+              next()
+            }
+          })
         })
       }),
   }
