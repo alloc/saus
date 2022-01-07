7a0e5bce3d8737484c5a738496aacf85efdac5d1
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-01-07 16:57:01 -0500

    refactor: use RawSourceMap type from `source-map`

--- src/bundle/sourceMap.ts
+++ src/bundle/sourceMap.ts
@@ -1,16 +1,11 @@
+import { RawSourceMap as SourceMap } from 'source-map'
+
+export type { SourceMap }
+
 export function toInlineSourceMap(map: SourceMap) {
   return (
     '\n//# ' +
     'sourceMappingURL=data:application/json;charset=utf-8;base64,' +
     Buffer.from(JSON.stringify(map), 'utf8').toString('base64')
   )
-}
-
-export interface SourceMap {
-  version: number
-  file: string
-  sources: string[]
-  sourcesContent: string[]
-  names: string[]
-  mappings: string
 }
