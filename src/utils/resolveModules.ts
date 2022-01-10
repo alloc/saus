5cccec14496b185a826d97a4655aa017cebcd12e
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-01-10 11:46:45 -0500

    fix: `resolveModules` return type
    Using the `ResolvedState` conditional type is not entirely accurate,
    but good enough for our needs

--- src/utils/resolveModules.ts
+++ src/utils/resolveModules.ts
@@ -1,7 +1,8 @@
+import type { ResolvedState } from '../core'
 import { unwrapDefault } from './unwrapDefault'
 
 export async function resolveModules<T extends Promise<any>[]>(
   ...modules: T
-): Promise<T> {
+): Promise<ResolvedState<T>> {
   return (await Promise.all(modules)).map(unwrapDefault) as any
 }
