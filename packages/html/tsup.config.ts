9e4494b27fb7547a31d60c0a7b14b75f98b9c107
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-01-13 14:44:52 -0500

    chore: fix tsup config

--- packages/html/tsup.config.ts
+++ packages/html/tsup.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'tsup'
+import { crawl } from 'recrawl-sync'
 
 export default defineConfig({
-  entry: ['src/*'],
+  entry: crawl('src', {
+    only: ['*.ts'],
+    skip: ['*.spec.*', 'test.ts'],
+    absolute: true,
+  }),
   format: ['cjs', 'esm'],
   bundle: false,
   sourcemap: true,
