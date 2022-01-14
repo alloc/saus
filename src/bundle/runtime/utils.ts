75899b68c789eed8b426a8ffb46fe9d1db31aff0
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-01-14 14:17:57 -0500

    chore: reorganize src/bundle/runtime folder

--- src/bundle/runtime/utils.ts
+++ src/bundle/runtime/utils.ts
@@ -1,75 +1,16 @@
-/* Utility functions copied from "vite/src/node/utils.ts" */
+import { ClientModule } from '../types'
+import config from './config'
 
-export function slash(p: string): string {
-  return p.replace(/\\/g, '/')
-}
+const htmlExtension = '.html'
+const indexHtmlSuffix = '/index.html'
 
-const queryRE = /\?.*$/s
-const hashRE = /#.*$/s
-
-export const cleanUrl = (url: string): string =>
-  url.replace(hashRE, '').replace(queryRE, '')
-
-export const externalRE = /^(https?:)?\/\//
-export const isExternalUrl = (url: string): boolean => externalRE.test(url)
-
-export const dataUrlRE = /^\s*data:/i
-export const isDataUrl = (url: string): boolean => dataUrlRE.test(url)
-
-interface ImageCandidate {
-  url: string
-  descriptor: string
-}
-const escapedSpaceCharacters = /( |\\t|\\n|\\f|\\r)+/g
-export async function processSrcSet(
-  srcs: string,
-  replacer: (arg: ImageCandidate) => Promise<string>
-): Promise<string> {
-  const imageCandidates: ImageCandidate[] = srcs
-    .split(',')
-    .map(s => {
-      const [url, descriptor] = s
-        .replace(escapedSpaceCharacters, ' ')
-        .trim()
-        .split(' ', 2)
-      return { url, descriptor }
-    })
-    .filter(({ url }) => !!url)
-
-  const ret = await Promise.all(
-    imageCandidates.map(async ({ url, descriptor }) => {
-      return {
-        url: await replacer({ url, descriptor }),
-        descriptor,
-      }
-    })
-  )
-
-  const url = ret.reduce((prev, { url, descriptor }, index) => {
-    descriptor = descriptor || ''
-    return (prev +=
-      url + ` ${descriptor}${index === ret.length - 1 ? '' : ', '}`)
-  }, '')
-
-  return url
-}
-
-export function checkPublicFile(
-  url: string,
-  { publicDir }: ResolvedConfig
-): string | undefined {
-  // note if the file is in /public, the resolver would have returned it
-  // as-is so it's not going to be a fully resolved path.
-  if (!publicDir || !url.startsWith('/')) {
-    return
-  }
-  const publicFile = path.join(publicDir, cleanUrl(url))
-  if (fs.existsSync(publicFile)) {
-    return publicFile
-  } else {
-    return
-  }
-}
+export const getModuleUrl = (mod: ClientModule) =>
+  config.base +
+  (mod.id.endsWith(htmlExtension)
+    ? mod.id.endsWith(indexHtmlSuffix)
+      ? mod.id.slice(0, -indexHtmlSuffix.length)
+      : mod.id.slice(0, -htmlExtension.length)
+    : mod.id)
 
 const cssLangs = `\\.(css|less|sass|scss|styl|stylus|pcss|postcss)($|\\?)`
 const cssLangRE = new RegExp(cssLangs)
