eb45efcbe59862582935c80afc0cf880a50ade1d
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-01-17 19:15:56 -0500

    fix: throw a clearer error when `loadPageModule` is given a bad route

--- src/bundle/runtime/loadPageModule.ts
+++ src/bundle/runtime/loadPageModule.ts
@@ -2,5 +2,10 @@ import { ssrRequire } from '../ssrModules'
 import routes from './routes'
 
 /** This overrides `loadPageModule` (exported by `saus/client`) in SSR environment. */
-export const loadPageModule = (routePath: string) =>
-  ssrRequire(routes[routePath])
+export const loadPageModule = (routePath: string) => {
+  const routeModuleUrl = routes[routePath]
+  if (!routeModuleUrl) {
+    throw Error(`Unknown route: "${routePath}"`)
+  }
+  return ssrRequire(routeModuleUrl)
+}
--- src/client/loadPageModule.ts
+++ src/client/loadPageModule.ts
@@ -3,7 +3,12 @@ import { getPagePath } from '../utils/getPagePath'
 import { loadClientState } from './state'
 import routes from './routes'
 
-export const loadPageModule = (routePath: string, routeParams?: RouteParams) =>
-  loadClientState(getPagePath(routePath, routeParams)).then(
-    () => import(/* @vite-ignore */ routes[routePath])
+export function loadPageModule(routePath: string, routeParams?: RouteParams) {
+  const routeModuleUrl = routes[routePath]
+  if (!routeModuleUrl) {
+    throw Error(`Unknown route: "${routePath}"`)
+  }
+  return loadClientState(getPagePath(routePath, routeParams)).then(
+    () => import(/* @vite-ignore */ routeModuleUrl)
   )
+}
