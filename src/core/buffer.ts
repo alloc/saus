ee2423ca4144065a84a80be0abd8b74e809394a1
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-01-27 23:26:39 -0500

    feat: add `unwrapBuffer` to saus/core

--- src/core/buffer.ts
+++ src/core/buffer.ts
@@ -2,3 +2,19 @@ import type { Buffer as IBuffer } from '../client/Buffer'
 
 declare class Buffer extends IBuffer {}
 export { Buffer }
+
+export type UnwrapBuffer<T> = T extends ArrayBuffer | Buffer
+  ? globalThis.Buffer
+  : T
+
+/**
+ * Get a string or Node buffer for compatibility with Node libraries.
+ */
+export function unwrapBuffer<T extends string | ArrayBuffer | Buffer>(
+  data: T
+): UnwrapBuffer<T> {
+  // Since we use Node buffers in SSR, it's usually just a type cast.
+  return typeof data == 'string' || global.Buffer.isBuffer(data)
+    ? (data as any)
+    : global.Buffer.from(data as ArrayBuffer)
+}
