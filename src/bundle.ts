ade65b8cf91891f403633a1ed73dca2c5e34fd05
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-02-10 16:10:35 -0500

    chore: move `preferExternal` to its own module

--- src/bundle.ts
+++ src/bundle.ts
@@ -14,7 +14,6 @@ import {
   dataToEsm,
   endent,
   extractClientFunctions,
-  SausContext,
 } from './core'
 import {
   BundleConfig,
@@ -26,6 +25,7 @@ import {
   preBundleSsrRuntime,
   resolveRouteImports,
 } from './core/bundle'
+import { preferExternal } from './core/bundle/preferExternal'
 import type { RuntimeConfig } from './core/config'
 import { debug } from './core/debug'
 import { bundleDir, clientDir, coreDir } from './core/paths'
@@ -42,7 +42,6 @@ import {
 import { routesPlugin } from './plugins/routes'
 import { Profiling } from './profiling'
 import { callPlugins } from './utils/callPlugins'
-import { findPackage } from './utils/findPackage'
 import { parseImports, serializeImports } from './utils/imports'
 import { relativeToCwd } from './utils/relativeToCwd'
 import { resolveMapSources, SourceMap } from './utils/sourceMap'
@@ -483,101 +482,6 @@ function defineNodeConstants(): vite.Plugin {
       })
       if (matched) {
         return { code, map: { mappings: '' } }
-      }
-    },
-  }
-}
-
-type ExternalDictation = { external: boolean; msg?: string }
-type PreferExternal = vite.Plugin & {
-  isExternal(id: string): ExternalDictation
-}
-
-function preferExternal(context: SausContext): PreferExternal {
-  const externalCache = new Map<string, boolean>()
-  const cjsResolve = context.config.createResolver({
-    asSrc: false,
-    isRequire: true,
-    mainFields: ['main'],
-    extensions: ['.js', '.cjs'],
-  })
-
-  function isExternal(id: string): ExternalDictation {
-    const isNodeModule = id.includes('/node_modules/')
-    if (!isNodeModule && id.startsWith(context.root + '/')) {
-      // Project files are resolved normally.
-      return { external: false }
-    }
-
-    // Ensure the resolved path is a CommonJS module,
-    // since the package might be ESM only.
-    if (!/\.c?js$/.test(id)) {
-      return { external: false }
-    }
-
-    let external = externalCache.get(id)
-    if (external !== undefined) {
-      return { external }
-    }
-
-    let result: ExternalDictation
-
-    // Modules using ESM syntax must be bundled.
-    const code = fs.readFileSync(id, 'utf8')
-    if (/^(im|ex)port /m.test(code)) {
-      result = { external: false, msg: 'is not commonjs' }
-    } else {
-      const pkgPath = findPackage(path.dirname(id))
-      if (!pkgPath) {
-        result = { external: false }
-      } else {
-        // Packages that depend on Saus must be bundled.
-        const pkg = require(pkgPath)
-        external = !(
-          'saus' in (pkg.dependencies || {}) ||
-          'saus' in (pkg.peerDependencies || {})
-        )
-        if (external) {
-          result = { external: true, msg: 'is external' }
-        } else {
-          result = { external: false, msg: 'depends on saus' }
-        }
-      }
-    }
-
-    externalCache.set(id, result.external)
-    return result
-  }
-
-  return {
-    name: 'saus:preferExternal',
-    enforce: 'pre',
-    isExternal,
-    async resolveId(id, importer) {
-      if (importer && /^[\w@]/.test(id)) {
-        if (builtinModules.includes(id)) {
-          return { id, external: true }
-        }
-        const log = (color: typeof kleur.blue, resultMsg: string) => {
-          debug(id + kleur.gray(` from ${importer}`))
-          debug(`  ${color(resultMsg)}`)
-        }
-        let resolved: string | undefined
-        try {
-          resolved = await cjsResolve(id, importer, undefined, true)
-        } catch (e) {
-          log(kleur.red, (e as any).message)
-          return null
-        }
-        if (!resolved) {
-          log(kleur.red, 'not found')
-          return null
-        }
-        const { external, msg } = isExternal(resolved)
-        if (msg) {
-          log(external ? kleur.green : kleur.yellow, msg)
-        }
-        return external ? { id: resolved, external: true } : null
       }
     },
   }
