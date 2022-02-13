280c3c82fd95d89b0753bcae3083302b0dfc18ec
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-02-13 18:01:51 -0500

    fix(bundle): regression with `debugBase` and BASE_URL export

--- src/client/hydrate.ts
+++ src/client/hydrate.ts
@@ -1,7 +1,7 @@
 import type { ClientState, RenderRequest } from '../core'
 import { globalCache } from '../runtime/cache'
 import { getPageFilename } from '../utils/getPageFilename'
-import { BASE_URL } from './constants'
+import { BASE_URL } from './baseUrl'
 import routes from './routes'
 
 export type HydrateFn = (request: RenderRequest) => void
