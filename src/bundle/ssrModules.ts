8921843e3e8e2e73e4452d2e247e287387f091bb
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-02-14 21:36:27 -0500

    fix(ssr): allow circular imports

--- src/bundle/ssrModules.ts
+++ src/bundle/ssrModules.ts
@@ -5,48 +5,53 @@ import { getCachedState } from '../runtime/getCachedState'
 const debug = createDebug('saus:cache')
 
 const ssrPrefix = 'saus-ssr:'
-const ssrModules: Record<string, ModuleGetter> = {}
+const ssrLoaderMap: Record<string, ModuleLoader> = {}
+const ssrExportsCache = new Map<string, any>()
+
+const importerStack: string[] = []
 
 /** Require a SSR module defined with `__d` */
 export function __requireAsync(id: string) {
-  const get = ssrModules[id]
-  if (!get) {
+  const loader = ssrLoaderMap[id]
+  if (!loader) {
     throw Error(`Module not found: "${id}"`)
   }
-  return get()
+  if (importerStack.includes(id)) {
+    return ssrExportsCache.get(id)
+  }
+  return getCachedState(ssrPrefix + id, async function ssrLoadModule() {
+    const exports: ModuleExports = {}
+    ssrExportsCache.set(id, exports)
+
+    // CommonJS loader
+    if (loader.length > 1) {
+      const module = { exports }
+      await loader(exports, module)
+      return module.exports
+    }
+
+    // ESM loader
+    await loader(exports)
+    if (!exports.__esModule) {
+      Object.defineProperty(exports, '__esModule', { value: true })
+    }
+    return exports
+  })
 }
 
 type ModuleExports = Record<string, any>
-type ModuleGetter = () => Promise<any>
 type ModuleLoader = (
   exports: ModuleExports,
-  module: { exports: ModuleExports }
+  module?: { exports: ModuleExports }
 ) => Promise<void>
 
 /** Define a SSR module with async loading capability */
 export const __d = (id: string, loader: ModuleLoader) =>
-  (ssrModules[id] = getCachedState.bind(
-    null,
-    ssrPrefix + id,
-    async function ssrLoadModule() {
-      const exports: ModuleExports = {}
-      // CommonJS loader
-      if (loader.length > 1) {
-        const module = { exports }
-        await loader(exports, module)
-        return module.exports
-      }
-      // @ts-ignore: ESM loader
-      await loader(exports)
-      if (!exports.__esModule) {
-        Object.defineProperty(exports, '__esModule', { value: true })
-      }
-      return exports
-    }
-  ))
+  (ssrLoaderMap[id] = loader)
 
 /** Clear all loaded SSR modules */
 export function ssrClearCache() {
   debug('Clearing the module cache')
   clearCachedState(key => key.startsWith(ssrPrefix))
+  ssrExportsCache.clear()
 }
