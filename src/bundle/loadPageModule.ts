91d45e83d2b01e53ec9fdb31a59d371a7475c2db
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-02-18 15:10:39 -0500

    fix(bundle): `loadPageModule` must pass client state to `unwrapModule` callback

--- src/bundle/loadPageModule.ts
+++ src/bundle/loadPageModule.ts
@@ -1,19 +1,23 @@
+import { getCachedState } from '../runtime/getCachedState'
+import { getPagePath } from '../utils/getPagePath'
 import { ssrImport } from './ssrModules'
 import routes from './routes'
 
 /** This overrides `loadPageModule` (exported by `saus/client`) in SSR environment. */
-export function loadPageModule(
+export async function loadPageModule(
   routePath: string,
-  _routeParams?: any,
-  unwrapModule?: (routeModule: any) => any
+  routeParams: any,
+  unwrapModule?: (routeModule: any, pageState: any) => any
 ) {
   const routeModuleUrl = routes[routePath]
   if (!routeModuleUrl) {
     throw Error(`Unknown route: "${routePath}"`)
   }
-  const routeModule = ssrImport(routeModuleUrl)
+  const routeModule = await ssrImport(routeModuleUrl)
   if (unwrapModule) {
-    return routeModule.then(unwrapModule)
+    const pagePath = getPagePath(routePath, routeParams)
+    const pageState = await getCachedState(pagePath)
+    return unwrapModule(routeModule, pageState)
   }
   return routeModule
 }
