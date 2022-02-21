89e936d1794669003060a37d909f8993344f6206
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-02-21 15:18:37 -0500

    fix(bundle): tree shaking bug in `createRendererChunk`

--- src/core/bundle/isolateRoutes.ts
+++ src/core/bundle/isolateRoutes.ts
@@ -35,6 +35,7 @@ import {
 } from '../index'
 import { getViteTransform } from '../viteTransform'
 import { findLiveBindings, LiveBinding, matchLiveBinding } from './liveBindings'
+import { createRendererChunk } from './rendererChunk'
 import { RouteImports } from './routeModules'
 
 export type IsolatedModuleMap = Record<string, IsolatedModule>
@@ -713,52 +714,4 @@ function isolateRenderers(
   })
 
   return rendererMap
-}
-
-/**
- * Each renderer gets its own chunk so it can be reloaded once
- * per rendered page.
- */
-function createRendererChunk(
-  type: 'render' | 'beforeRender',
-  func: ClientFunction,
-  editor: MagicString
-): IsolatedModule {
-  const preservedRanges: [number, number][] = []
-  const preserveRange = (p: { node: t.Node }) =>
-    preservedRanges.push([p.node.start!, p.node.end!])
-
-  const callee = func.callee!
-  let callExpr = callee.findParent(p =>
-    p.isCallExpression()
-  )! as babel.NodePath<t.CallExpression>
-
-  // Preserve the render/beforeRender call and any chained calls.
-  const callStmt = callExpr.getStatementParent()!
-  preserveRange(callStmt)
-
-  // Preserve any referenced statements.
-  while ((callExpr = callExpr.findParent(p => p.isCallExpression()) as any)) {
-    resolveReferences(callExpr.get('arguments')).forEach(preserveRange)
-  }
-  resolveReferences(callee).forEach(preserveRange)
-  func.referenced.forEach(preserveRange)
-
-  // Sort the preserved ranges in order of appearance.
-  preservedRanges.sort(([a], [b]) => a - b)
-
-  // Remove the unused ranges.
-  editor.remove(0, preservedRanges[0][0])
-  preservedRanges.forEach(([, removedStart], i) => {
-    const nextRange = preservedRanges[i + 1]
-    const removedEnd = nextRange ? nextRange[0] - 1 : editor.original.length
-    if (removedStart < removedEnd) {
-      editor.remove(removedStart, removedEnd)
-    }
-  })
-
-  return {
-    code: editor.toString(),
-    map: editor.generateMap(),
-  }
 }
