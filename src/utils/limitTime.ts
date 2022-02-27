d84e05999ac8da4bedcd5aa06d0cb6eaaa363789
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-02-27 13:31:59 -0500

    fix: avoid weird bug with `Promise.race`
    Errors were being logged even if caught

--- src/utils/limitTime.ts
+++ src/utils/limitTime.ts
@@ -5,15 +5,15 @@ export function limitTime<T>(
   secs: number,
   reason?: string
 ): Promise<T> {
-  if (secs <= 0 || !(promise instanceof Promise)) {
+  if (!(promise instanceof Promise)) {
     return Promise.resolve(promise)
   }
+  if (secs <= 0) {
+    return promise
+  }
   const trace = Error(reason || 'Timed out')
-  return Promise.race([
-    promise,
-    new Promise<T>((_, reject) => {
-      const id = setTimeout(() => reject(trace), secs * 1e3)
-      promise.finally(() => clearTimeout(id))
-    }),
-  ])
+  return new Promise<T>((resolve, reject) => {
+    const id = setTimeout(() => reject(trace), secs * 1e3)
+    promise.then(resolve, reject).finally(() => clearTimeout(id))
+  })
 }
