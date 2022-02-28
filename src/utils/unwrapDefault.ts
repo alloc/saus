839ac3b8196e024adee74e620e7a1ea4358b1517
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-02-28 14:22:54 -0500

    feat: improved type for `unwrapDefault`

--- src/utils/unwrapDefault.ts
+++ src/utils/unwrapDefault.ts
@@ -1,3 +1,6 @@
+export function unwrapDefault<T = any>(module: { default: T }): T
+export function unwrapDefault<T = any>(module: Promise<T>): never
+export function unwrapDefault<T = any>(module: any): T
 export function unwrapDefault<T = any>(module: any): T {
   const exported = Object.keys(module)
   if (exported.length == 1 && exported[0] == 'default') {
