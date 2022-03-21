2c04085d8d7b1597b4e261b15342edb48dae4e48
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-03-21 19:19:22 -0400

    fix: TS4025 error when compiling bundle/index.d.ts

--- env/client.d.ts
+++ env/client.d.ts
@@ -1,13 +1,3 @@
 /// <reference types="vite/client" />
 
-declare const saus: {
-  /**
-   * The `saus.defaultPath` option set in Vite config.
-   * @default "/404"
-   */
-  defaultPath: string
-  /**
-   * Equals `true` after the page is fully hydrated.
-   */
-  hydrated: boolean
-}
+declare const saus: import('../src/client/context').ClientContext
--- src/client/context.ts
+++ src/client/context.ts
@@ -1,6 +1,18 @@
+export interface ClientContext {
+  /**
+   * The `saus.defaultPath` option set in Vite config.
+   * @default "/404"
+   */
+  defaultPath: string
+  /**
+   * Equals `true` after the page is fully hydrated.
+   */
+  hydrated: boolean
+}
+
 type Constants = 'defaultPath'
 
-const context: Omit<typeof saus, Constants> = {
+const context: Omit<ClientContext, Constants> = {
   hydrated: false,
 }
 
