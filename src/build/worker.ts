0b8099c053c703a478d4c0dcab7682ed35e85723
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-03-22 13:46:46 -0400

    fix(types): allow void return in `BuildWorker.renderPage`

--- src/build/worker.ts
+++ src/build/worker.ts
@@ -2,7 +2,7 @@ import { workerData } from 'worker_threads'
 import { loadPageFactory } from './pageFactory'
 
 export interface BuildWorker {
-  renderPage(pageUrl: string): Promise<void>
+  renderPage(pageUrl: string): Promise<void> | void
   destroy?: () => Promise<void>
 }
 
