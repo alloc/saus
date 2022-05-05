0aec458cff9b68e1b040618b5dec2a7ab20bbb20
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-05-05 10:38:35 -0400

    wip

--- test/parse.test.ts
+++ test/parse.test.ts
@@ -1,18 +1,24 @@
 import { test, expect } from 'vitest'
+import * as crypto from 'crypto'
 import * as path from 'path'
 import * as fs from 'fs'
+
+// @ts-ignore
+globalThis.crypto = crypto
+console.log('crypto.getRandomValues =>', globalThis.crypto.getRandomValues)
 
 const parsers = fs.readdirSync('./parsers')
 const fixtures = fs.readdirSync('./fixtures')
 
 for (const parser of parsers) {
   const parse = await import('../parsers/' + parser)
-  console.log({ [parser]: parse })
 
   for (const fixture of fixtures) {
     const fixtureId = path.resolve('../fixtures/', fixture, 'index.' + parser)
     if (!fs.existsSync(fixtureId)) continue
 
-    test('parse : ' + fixture + ' : ' + parser, () => {})
+    test('parse : ' + fixture + ' : ' + parser, () => {
+      console.log({ [parser]: parse })
+    })
   }
 }
