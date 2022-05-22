984239586fd564d3e2123a86ce0c42edca6499f0
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-05-22 00:18:51 -0400

    fix(client): import types only from src/core/client

--- src/client/loadPageState.ts
+++ src/client/loadPageState.ts
@@ -1,4 +1,4 @@
-import { CommonClientProps } from '../core/client'
+import type { CommonClientProps } from '../core/client'
 import { getCachedState } from '../runtime/getCachedState'
 import { getPageFilename } from '../utils/getPageFilename'
 import { unwrapDefault } from '../utils/unwrapDefault'
