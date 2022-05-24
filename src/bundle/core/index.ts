fd26649f51554544d193c4b181791178bd8b292f
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-05-23 23:55:20 -0400

    fix(bundle): call `setRoutesModule` and `setRenderModule` immediately

--- src/bundle/core/index.ts
+++ src/bundle/core/index.ts
@@ -1,6 +1,13 @@
 // Redirect "saus/core" imports here.
+import { setRenderModule, setRoutesModule } from '../../core/global'
+import { context } from '../context'
 
 export * from '../../core/api'
+
+// In SSR bundles, these globals are mutated at the top level
+// immediately, so they need to be defined now.
+setRoutesModule(context)
+setRenderModule(context)
 
 // This is also exported by "saus/src/core/client" but we
 // want to avoid processing that module, since it has heavy
