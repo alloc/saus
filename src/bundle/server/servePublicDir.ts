6b12d0ba19b00677beb253d6d69013b0e70cda9d
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-05-25 13:39:59 -0400

    feat: add Cache-Control options to `servePublicDir`

--- src/bundle/server/servePublicDir.ts
+++ src/bundle/server/servePublicDir.ts
@@ -3,17 +3,30 @@ import fs from 'fs'
 import { gray } from 'kleur/colors'
 import * as mime from 'mrmime'
 import path from 'path'
-import type { RuntimeConfig } from '../../core/config'
+import runtimeConfig from '../core/runtimeConfig'
 import { connect } from './connect'
 import { debug } from './debug'
 
-export const servePublicDir = (
-  config: RuntimeConfig,
-  publicDir: string = config.publicDir,
-  ignore = /^$/
-): connect.Middleware =>
-  async function servePublicFile(req, res, next) {
-    const fileName = req.url.slice(config.base.length)
+interface Options {
+  /** @default runtimeConfig.publicDir */
+  root?: string
+  /** Prevent certain files from being served */
+  ignore?: RegExp
+  /**
+   * Set the `max-age` Cache-Control directive. \
+   * Set to `Infinity` to use the `immutable` directive.
+   */
+  maxAge?: number
+  /** Use the `stale-while-revalidate` cache strategy */
+  swr?: boolean | number
+}
+
+export function servePublicDir(options: Options = {}): connect.Middleware {
+  const { root: publicDir = runtimeConfig.publicDir, ignore = /^$/ } = options
+  const cacheControl = resolveCacheControl(options)
+
+  return async function servePublicFile(req, res, next) {
+    const fileName = req.url.slice(runtimeConfig.base.length)
     if (ignore.test(fileName)) {
       return next()
     }
@@ -23,6 +36,7 @@ export const servePublicDir = (
       res.writeHead(200, {
         ETag: etag(content, { weak: true }),
         'Content-Type': mime.lookup(req.url) || 'application/octet-stream',
+        'Cache-Control': cacheControl,
       })
       res.write(content)
       res.end()
@@ -33,3 +47,20 @@ export const servePublicDir = (
       throw e
     }
   }
+}
+
+function resolveCacheControl(options: Options) {
+  const cacheControl = ['public']
+  if (options.maxAge !== undefined) {
+    cacheControl.push(
+      isFinite(options.maxAge) ? 'max-age=' + options.maxAge : 'immutable'
+    )
+  }
+  if (options.swr !== undefined && options.swr !== false) {
+    cacheControl.push(
+      'stale-while-revalidate' +
+        (typeof options.swr == 'number' ? '=' + options.swr : '')
+    )
+  }
+  return cacheControl.join(', ')
+}
