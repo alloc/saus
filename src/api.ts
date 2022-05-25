70850e3cb3268d89e1424d18c8cc7a751c441725
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-05-25 11:40:39 -0400

    feat: export `Endpoint` type from "saus" entry point

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,7 @@
 // These exports are suitable to import from modules that
 // run in SSR bundles and/or during static builds.
 export { logRequests } from './app/logRequests'
+export { Endpoint } from './core/endpoint'
 export { includeState } from './core/includeState'
 export { beforeRender } from './core/render'
 export { RuntimeHook, setup } from './core/setup'
