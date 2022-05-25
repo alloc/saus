d928c47a531b44e1da1679dc879c8af213f3388f
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-05-25 11:32:08 -0400

    feat: add `elapsed` option to `logRequests`

--- src/app/logRequests.ts
+++ src/app/logRequests.ts
@@ -1,4 +1,5 @@
 import * as kleur from 'kleur/colors'
+import { Endpoint } from '../core/endpoint'
 import { setup } from '../core/setup'
 import type { Headers } from '../http'
 import { onRequest, onResponse } from '../routes'
@@ -8,38 +9,46 @@ export const logRequests = (
     request?: { headers?: boolean; prefix?: string }
     response?: { headers?: boolean; prefix?: string }
     timestamp?: boolean
+    elapsed?: boolean
   } = {}
 ) =>
   // Use `setup` to attach our hooks as late as possible.
   setup(() => {
+    const timing = new WeakMap<Endpoint.Request, number>()
+
     onRequest('pre', req => {
       if (options.timestamp) {
         printTimestamp()
       }
+      const method = req.method.toUpperCase()
       console.log(
-        kleur.yellow(
-          (options.request?.prefix || '▶︎') + ' ' + req.method.toUpperCase()
-        ),
+        kleur.yellow((options.request?.prefix || '▶︎') + ' ' + method),
         req.path
       )
       if (options.request?.headers) {
         printHeaders(req.headers)
       }
+      timing.set(req, Date.now())
     })
 
     onResponse((req, [status, headers]) => {
-      const statusColor = /^[23]/.test('' + status) ? kleur.green : kleur.red
-      const contentLength = headers && headers['Content-Length']
       if (options.timestamp) {
         printTimestamp()
       }
-      console.log(
+      const elapsed = options.elapsed && (Date.now() - timing.get(req)!) / 1e3
+      const statusColor = /^[23]/.test('' + status) ? kleur.green : kleur.red
+      const message = [
         statusColor((options.response?.prefix || '◀︎') + ' ' + status),
         req.path,
-        contentLength
-          ? kleur.gray((+contentLength / 1024).toFixed(2) + 'KiB')
-          : ''
-      )
+      ]
+      const contentLength = headers && headers['Content-Length']
+      if (contentLength) {
+        message.push(kleur.gray((+contentLength / 1024).toFixed(2) + 'KiB'))
+      }
+      if (options.elapsed) {
+        message.push(kleur.gray('in ' + (elapsed as number).toFixed(3) + 's'))
+      }
+      console.log(...message)
       if (headers && options.response?.headers) {
         printHeaders(headers)
       }
