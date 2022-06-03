fc6804dd66926f7f4b5aad10f3af37d9aba6c598
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-06-03 19:21:54 -0400

    refactor: stop using `http.RequestOptions`
    1. It can change based on `@types/node` version
    2. Sometimes it leads to `unknown` values (not sure why)

--- src/http/types.ts
+++ src/http/types.ts
@@ -1,25 +1,28 @@
-import http from 'http'
+import http, { Agent } from 'http'
 import { httpMethods } from '../utils/httpMethods'
 
-export { BufferLike } from '../app/types'
+export type { BufferLike } from '../app/types'
 
 export type HttpMethod = typeof httpMethods[number]
 
-export interface HttpOptions extends http.RequestOptions {
-  hash?: string
-  search?: string
-  pathname?: string
-  href?: string
+export interface HttpOptions {
+  agent?: Agent | boolean | undefined
   /**
    * If the response has a status code *not* between 200 and 399,
    * resolve the promise normally instead of rejecting it.
    */
   allowBadStatus?: boolean
+  hash?: string
+  href?: string
+  pathname?: string
+  search?: string
+  signal?: AbortSignal | undefined
   /**
    * Instead of buffering the response data into memory, this
    * function will be called with the response object.
    */
   sink?: (resp: http.IncomingMessage) => void
+  timeout?: number | undefined
 }
 
 export const URL = (globalThis as any).URL as typeof import('url').URL
