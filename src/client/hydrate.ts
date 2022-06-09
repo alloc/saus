d4ff9d3391595c3564c654fdcf2f6505062877a2
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-06-09 15:42:17 -0400

    fix(client): leave `routePath` as-is when hydrating

--- src/client/hydrate.ts
+++ src/client/hydrate.ts
@@ -1,7 +1,6 @@
 import type { CommonClientProps, RenderRequest } from '../core'
 import { globalCache } from '../runtime/cache'
 import { getPageFilename } from '../utils/getPageFilename'
-import { BASE_URL } from './baseUrl'
 import routes from './routes'
 
 export type HydrateFn = (request: RenderRequest) => Promise<void> | void
@@ -16,8 +15,7 @@ export async function hydrate(
   if (import.meta.env.DEV && !runHydration) {
     throw Error(`[saus] "onHydrate" must be called before "hydrate"`)
   }
-  const routePath = props.routePath.replace('/', BASE_URL)
-  routes[routePath] = routeModuleUrl
+  routes[props.routePath] = routeModuleUrl
   const path = location.pathname
   globalCache.loaded[path] = [props]
   await runHydration({
