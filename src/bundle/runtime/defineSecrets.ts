dec9c0a587c6b62f19abc59b5c45840924bb7f5b
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-08-05 13:45:13 -0400

    fix: defineSecrets runtime proxy

--- src/bundle/runtime/defineSecrets.ts
+++ src/bundle/runtime/defineSecrets.ts
@@ -2,15 +2,11 @@ import { deployedEnv } from '@/runtime/deployedEnv'
 import type { SecretMap } from '../../secrets/types'
 
 export function defineSecrets(secrets: SecretMap) {
-  const keys: Record<string, string> = {}
-  for (const key in secrets) {
-    keys[secrets[key]] = key
-  }
   // Secret access is forwarded to the deployedEnv object,
   // which should be populated by external packages.
   return new Proxy(deployedEnv, {
     get(_, key: string) {
-      key = keys[key]
+      key = secrets[key]
       if (key) {
         const value = deployedEnv[key]
         if (value === undefined) {
