0298c7c9c8d8a6312de91b956b800b5e8ae17c77
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-09-19 15:08:52 -0400

    feat(client): add AbortSignal argument to `loadPageClient`
    …and call injectLinkTag with the route client before loading page state

--- src/core/client/pageClient.ts
+++ src/core/client/pageClient.ts
@@ -2,10 +2,10 @@ import { getPagePath } from '@/utils/getPagePath'
 import { AnyToObject } from '@/utils/types'
 import type { CommonClientProps, RouteParams } from '../core'
 import { RouteEntry } from '../routeEntries'
-import { applyHead } from './head'
+import { noop } from '../utils/noop'
+import { applyHead, injectLinkTag } from './head'
 import { loadPageState } from './loadPageState'
 import routes from './routes'
-import { noop } from '../utils/noop'
 
 export interface PageClient<
   Props extends object = any,
@@ -27,7 +27,27 @@ export async function loadPageClient<
 >(
   routePath: string,
   routeParams?: RouteParams
-): Promise<PageClient<Props, Module, RenderResult>> {
+): Promise<PageClient<Props, Module, RenderResult>>
+
+export async function loadPageClient<
+  Props extends object = any,
+  Module extends object = any,
+  RenderResult = any
+>(
+  routePath: string,
+  routeParams: RouteParams | null | undefined,
+  signal: AbortSignal
+): Promise<PageClient<Props, Module, RenderResult> | null>
+
+export async function loadPageClient<
+  Props extends object = any,
+  Module extends object = any,
+  RenderResult = any
+>(
+  routePath: string,
+  routeParams?: RouteParams | null,
+  signal?: AbortSignal
+): Promise<PageClient<Props, Module, RenderResult> | null> {
   const clientUrl = routes[routePath]
   if (!clientUrl) {
     throw Error(`Unknown route: "${routePath}"`)
@@ -39,7 +59,12 @@ export async function loadPageClient<
       : saus.defaultPath
 
   try {
+    injectLinkTag(clientUrl)
+
     const pageProps = await loadPageState<Props>(pagePath)
+    if (signal?.aborted) {
+      return null
+    }
 
     // Add any desired <link> tags and update the <title> tag
     // before executing the route entry.
@@ -51,6 +76,9 @@ export async function loadPageClient<
       props: pageProps,
     }
   } catch (error: any) {
+    if (signal?.aborted) {
+      return null
+    }
     if (error.code == 'PAGE_404') {
       if (routes['default'] && routePath !== 'default') {
         return loadPageClient('default')
