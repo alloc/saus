a9bbf8eb9c4e2489a15a9b532eadc127d8e40cc2
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-09-29 20:24:04 -0400

    fix: support `Uint8Array` in `murmurHash` wrapper

--- src/core/utils/murmur3.ts
+++ src/core/utils/murmur3.ts
@@ -1,6 +1,20 @@
 import { murmurHash as hash } from 'ohash'
 
-export function murmurHash(s: string) {
-  const padding = (4 - (s.length & 3)) & 3
-  return hash(s.padEnd(s.length + padding, '\0'))
+declare const TextEncoder: any
+
+const nullChar = '\0'
+const nullByte: Uint8Array = new TextEncoder().encode(nullChar)
+
+export function murmurHash(data: string | Uint8Array) {
+  const padding = (4 - (data.length & 3)) & 3
+  if (typeof data == 'string') {
+    data = data.padEnd(data.length + padding, nullChar)
+    return hash(data)
+  }
+  const resized = new Uint8Array(data.byteLength + padding)
+  resized.set(data)
+  for (let i = 0; i < padding; i++) {
+    resized.set(nullByte, data.length + i)
+  }
+  return hash(resized)
 }
