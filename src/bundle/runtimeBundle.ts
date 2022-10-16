9739161cfe191f0943ed9bafa467ae60a91bd1e6
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-10-16 18:47:16 -0400

    wipppp

--- src/bundle/runtimeBundle.ts
+++ src/bundle/runtimeBundle.ts
@@ -168,7 +168,7 @@ async function compileSsrRuntime(context: BundleContext) {
               './core/index.mjs',
               './core/context.mjs',
               './deploy/index.mjs',
-              './utils/babel/index.mjs',
+              './utils/babel.mjs',
             ]),
           ],
           configFile: false,
