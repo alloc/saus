a545f14acfb7577fadcdeceeea2cf5f773ea54fe
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-10-16 15:52:15 -0400

    wipppp

--- src/utils/babel.ts
+++ src/utils/babel.ts
@@ -2,5 +2,7 @@ import * as babel from '@babel/core'
 import { NodePath, types as t } from '@babel/core'
 
 export { Bundle as MagicBundle, default as MagicString } from 'magic-string'
+export { getBabelConfig } from './babel/config'
 export { getBabelProgram } from './babel/program'
+export { transformAsync, transformSync } from './babel/transform'
 export { NodePath, t, babel }
